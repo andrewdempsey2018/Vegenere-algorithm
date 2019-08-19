1ce2570f641062d26a4f801e02bd970806a41396
Andrew Dempsey <andrewdempsey2016@gmail.com>
2019-08-19 21:41:12 +0100

    Signed-off-by: Andrew Dempsey <andrewdempsey2016@gmail.com>
    Comments improved

--- vigenere.js
+++ vigenere.js
@@ -1,123 +1,162 @@
-function vegenere(plainText, key)
+var vegenereSquareLowerCase = ["abcdefghijklmnopqrstuvwxyz",
+	"bcdefghijklmnopqrstuvwxyza",
+	"cdefghijklmnopqrstuvwxyzab",
+	"defghijklmnopqrstuvwxyzabc",
+	"efghijklmnopqrstuvwxyzabcd",
+	"fghijklmnopqrstuvwxyzabcde",
+	"ghijklmnopqrstuvwxyzabcdef",
+	"hijklmnopqrstuvwxyzabcdefg",
+	"ijklmnopqrstuvwxyzabcdefgh",
+	"jklmnopqrstuvwxyzabcdefghi",
+	"klmnopqrstuvwxyzabcdefghij",
+	"lmnopqrstuvwxyzabcdefghijk",
+	"mnopqrstuvwxyzabcdefghijkl",
+	"nopqrstuvwxyzabcdefghijklm",
+	"opqrstuvwxyzabcdefghijklmn",
+	"pqrstuvwxyzabcdefghijklmno",
+	"qrstuvwxyzabcdefghijklmnop",
+	"rstuvwxyzabcdefghijklmnopq",
+	"stuvwxyzabcdefghijklmnopqr",
+	"tuvwxyzabcdefghijklmnopqrs",
+	"uvwxyzabcdefghijklmnopqrst",
+	"vwxyzabcdefghijklmnopqrstu",
+	"wxyzabcdefghijklmnopqrstuv",
+	"xyzabcdefghijklmnopqrstuvw",
+	"yzabcdefghijklmnopqrstuvwx",
+	"zabcdefghijklmnopqrstuvwxy"];
+
+var vegenereSquareUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	"BCDEFGHIJKLMNOPQRSTUVWXYZA",
+	"CDEFGHIJKLMNOPQRSTUVWXYZAB",
+	"DEFGHIJKLMNOPQRSTUVWXYZABC",
+	"EFGHIJKLMNOPQRSTUVWXYZABCD",
+	"FGHIJKLMNOPQRSTUVWXYZABCDE",
+	"GHIJKLMNOPQRSTUVWXYZABCDEF",
+	"HIJKLMNOPQRSTUVWXYZABCDEFG",
+	"IJKLMNOPQRSTUVWXYZABCDEFGH",
+	"JKLMNOPQRSTUVWXYZABCDEFGHI",
+	"KLMNOPQRSTUVWXYZABCDEFGHIJ",
+	"LMNOPQRSTUVWXYZABCDEFGHIJK",
+	"MNOPQRSTUVWXYZABCDEFGHIJKL",
+	"NOPQRSTUVWXYZABCDEFGHIJKLM",
+	"OPQRSTUVWXYZABCDEFGHIJKLMN",
+	"PQRSTUVWXYZABCDEFGHIJKLMNO",
+	"QRSTUVWXYZABCDEFGHIJKLMNOP",
+	"RSTUVWXYZABCDEFGHIJKLMNOPQ",
+	"STUVWXYZABCDEFGHIJKLMNOPQR",
+	"TUVWXYZABCDEFGHIJKLMNOPQRS",
+	"UVWXYZABCDEFGHIJKLMNOPQRST",
+	"VWXYZABCDEFGHIJKLMNOPQRSTU",
+	"WXYZABCDEFGHIJKLMNOPQRSTUV",
+	"XYZABCDEFGHIJKLMNOPQRSTUVW",
+	"YZABCDEFGHIJKLMNOPQRSTUVWX",
+	"ZABCDEFGHIJKLMNOPQRSTUVWXY"];
+
+function processKey(key, targetString)
 {
-	var vegenereSquareLowerCase = ["abcdefghijklmnopqrstuvwxyz",
-		"bcdefghijklmnopqrstuvwxyza",
-		"cdefghijklmnopqrstuvwxyzab",
-		"defghijklmnopqrstuvwxyzabc",
-		"efghijklmnopqrstuvwxyzabcd",
-		"fghijklmnopqrstuvwxyzabcde",
-		"ghijklmnopqrstuvwxyzabcdef",
-		"hijklmnopqrstuvwxyzabcdefg",
-		"ijklmnopqrstuvwxyzabcdefgh",
-		"jklmnopqrstuvwxyzabcdefghi",
-		"klmnopqrstuvwxyzabcdefghij",
-		"lmnopqrstuvwxyzabcdefghijk",
-		"mnopqrstuvwxyzabcdefghijkl",
-		"nopqrstuvwxyzabcdefghijklm",
-		"opqrstuvwxyzabcdefghijklmn",
-		"pqrstuvwxyzabcdefghijklmno",
-		"qrstuvwxyzabcdefghijklmnop",
-		"rstuvwxyzabcdefghijklmnopq",
-		"stuvwxyzabcdefghijklmnopqr",
-		"tuvwxyzabcdefghijklmnopqrs",
-		"uvwxyzabcdefghijklmnopqrst",
-		"vwxyzabcdefghijklmnopqrstu",
-		"wxyzabcdefghijklmnopqrstuv",
-		"xyzabcdefghijklmnopqrstuvw",
-		"yzabcdefghijklmnopqrstuvwx",
-		"zabcdefghijklmnopqrstuvwxy"];
+	while(key.length < targetString.length)
+	{
+		key += key;
+	}
 
-	var vegenereSquareUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		"BCDEFGHIJKLMNOPQRSTUVWXYZA",
-		"CDEFGHIJKLMNOPQRSTUVWXYZAB",
-		"DEFGHIJKLMNOPQRSTUVWXYZABC",
-		"EFGHIJKLMNOPQRSTUVWXYZABCD",
-		"FGHIJKLMNOPQRSTUVWXYZABCDE",
-		"GHIJKLMNOPQRSTUVWXYZABCDEF",
-		"HIJKLMNOPQRSTUVWXYZABCDEFG",
-		"IJKLMNOPQRSTUVWXYZABCDEFGH",
-		"JKLMNOPQRSTUVWXYZABCDEFGHI",
-		"KLMNOPQRSTUVWXYZABCDEFGHIJ",
-		"LMNOPQRSTUVWXYZABCDEFGHIJK",
-		"MNOPQRSTUVWXYZABCDEFGHIJKL",
-		"NOPQRSTUVWXYZABCDEFGHIJKLM",
-		"OPQRSTUVWXYZABCDEFGHIJKLMN",
-		"PQRSTUVWXYZABCDEFGHIJKLMNO",
-		"QRSTUVWXYZABCDEFGHIJKLMNOP",
-		"RSTUVWXYZABCDEFGHIJKLMNOPQ",
-		"STUVWXYZABCDEFGHIJKLMNOPQR",
-		"TUVWXYZABCDEFGHIJKLMNOPQRS",
-		"UVWXYZABCDEFGHIJKLMNOPQRST",
-		"VWXYZABCDEFGHIJKLMNOPQRSTU",
-		"WXYZABCDEFGHIJKLMNOPQRSTUV",
-		"XYZABCDEFGHIJKLMNOPQRSTUVW",
-		"YZABCDEFGHIJKLMNOPQRSTUVWX",
-		"ZABCDEFGHIJKLMNOPQRSTUVWXY"];
+	if(key.length > targetString.length)
+	{
+		var charactersOver = key.length - targetString.length;
 
-		var cipherText = "";
+		key = key.slice(0, -charactersOver);
+	}
 
-		key = processKey(key);
+	return key;
+}
 
-		for(var i = 0; i < plainText.length; i++)
+function getRow(character)
+{
+	var row = 0;
+
+	character = character.toLowerCase();
+
+	for(var i = 0; i < vegenereSquareLowerCase.length; i++)
+	{
+		if(vegenereSquareLowerCase[i].charAt(0) == character)
 		{
-			/* sat evening check that charcode is uncode for a-z small and A-Z caps
-			so we can ignore spaces and symbols */
-			if(plainText.charCodeAt(i) >= 97 && plainText.charCodeAt(i) <= 122)
-			{
-			    cipherText += vegenereSquareLowerCase[getRow(key[i])].charAt(getColumn(plainText[i]));
-			}
-			else if(plainText.charCodeAt(i) >= 65 && plainText.charCodeAt(i) <= 90)
-			{
-				cipherText += vegenereSquareUpperCase[getRow(key[i])].charAt(getColumn(plainText[i]));
-			}
-			else
-			{
-				cipherText += plainText[i];
-			}
+			row = i;
+			break;
+		}
+	}
+	
+	return row;
+}
+
+function getColumnEncode(character)
+{
+	character = character.toLowerCase();
+
+	return vegenereSquareLowerCase[0].indexOf(character);
+}
+
+function getColumnDecode(character, row)
+{
+	character = character.toLowerCase();
+
+	return vegenereSquareLowerCase[row].indexOf(character);
+}
+
+function vegenereEncode(plainText, key)
+{
+    var cipherText = "";
+
+    key = processKey(key, plainText);
+
+    for(var i = 0; i < plainText.length; i++)
+    {
+		if(plainText.charCodeAt(i) >= 97 && plainText.charCodeAt(i) <= 122)
+		{
+		    cipherText += vegenereSquareLowerCase[getRow(key[i])].charAt(getColumnEncode(plainText[i]));
+		}
+		else if(plainText.charCodeAt(i) >= 65 && plainText.charCodeAt(i) <= 90)
+		{
+			cipherText += vegenereSquareUpperCase[getRow(key[i])].charAt(getColumnEncode(plainText[i]));
+		}
+		else
+		{
+			cipherText += plainText[i];
+		}
 			
-		}
+	}
 
-		function processKey(key)
+	return cipherText;
+}
+
+function vegenereDecode(cipherText, key)
+{
+    var plainText = "";
+
+	key = processKey(key, cipherText);
+
+	for(var i = 0; i < cipherText.length; i++)
+	{
+		if(cipherText.charCodeAt(i) >= 97 && cipherText.charCodeAt(i) <= 122)
 		{
-			while(key.length < plainText.length)
-			{
-				key += key;
-			}
+		    //plainText += vegenereSquareLowerCase[getRow(key[i])].charAt(getColumnEncode(cipherText[i]));
+			
+			var rowA = getRow(key[i]);
+			var colA = getColumnDecode(cipherText[i], rowA);
 
-			if(key.length > plainText.length)
-			{
-				var charactersOver = key.length - plainText.length;
-
-				key = key.slice(0, -charactersOver);
-			}
-
-			return key;
+			plainText += vegenereSquareLowerCase[0].charAt(colA);
 		}
-
-		function getRow(character)
+		else if(cipherText.charCodeAt(i) >= 65 && cipherText.charCodeAt(i) <= 90)
 		{
-			var row = 0;
+			var rowA = getRow(key[i]);
+			var colA = getColumnDecode(cipherText[i], rowA);
+
+			plainText += vegenereSquareUpperCase[0].charAt(colA);
+		}
+		else
+		{
+			plainText += cipherText[i];
+		}
 		
-			character = character.toLowerCase();
+	}
 
-			for(var i = 0; i < vegenereSquareLowerCase.length; i++)
-			{
-				if(vegenereSquareLowerCase[i].charAt(0) == character)
-				{
-					row = i;
-					break;
-				}
-			}
-			
-			return row;
-		}
-
-		function getColumn(character)
-		{
-			character = character.toLowerCase();
-
-			return vegenereSquareLowerCase[0].indexOf(character);
-		}
-
-		return cipherText;
+	return plainText;
 }
